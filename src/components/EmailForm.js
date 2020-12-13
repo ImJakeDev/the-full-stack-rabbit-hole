f876f8af60cf46e1377849b71c340b494a6c3a13
Jake Schaffer <jake.schaffer@pxdbproject.com>
2020-12-13 11:18:40 -0600

    updated netlify attribute

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -19,7 +19,7 @@ export class EmailForm extends Component {
   render() {
     const { message } = this.state;
     return (
-      <form id="signup-form" onSubmit={this.onSubmit} method="post" action="#" name="email-subscription" netlify>
+      <form id="signup-form" onSubmit={this.onSubmit} method="post" name="email-subscription" data-netlify="true" >
         <input
           type="email"
           name="email"
